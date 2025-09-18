b0933fb5692f2727aaa1730d04f24b076bb08a8c
BenFielder <ben.fielderwork@gmail.com>
2025-09-18 10:06:49 +0100

    Add backend using S3 bucket

--- versions.tf
+++ versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.11.0"
 
+  backend "s3" {
+    bucket = "madetech-bfielder-tfstate-bucket"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
