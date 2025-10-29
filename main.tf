a13a7ef1e441ecb3cbbd08f06a043f727e8c6266
BenFielder <ben.fielderwork@gmail.com>
2025-10-29 15:10:50 +0000

    Add outputs for dns name and updates to point to correct ECS image

--- main.tf
+++ main.tf
@@ -3,29 +3,29 @@ provider "aws" {
 }
 
 data "aws_availability_zones" "available" {
-    state = "available"
+  state = "available"
 }
 
 locals {
-  name = "${basename(path.cwd)}-${var.app_name}"
+  name               = "${basename(path.cwd)}-${var.app_name}"
   availability_zones = slice(data.aws_availability_zones.available.names, 0, 3)
-  
+
   container_name = "bfielder-container-test"
   container_port = 3000
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "6.0.1"
 
   cidr = "10.0.0.0/16"
   name = "bfielder-test-vpc"
 
-  azs = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway   = true
   enable_dns_hostnames = true
 
 }
@@ -35,18 +35,33 @@ module "vpc" {
 #Load balancer
 module "alb" {
   depends_on = [module.vpc]
-  source = "terraform-aws-modules/alb/aws"
-  version = "9.17.0"
+  source     = "terraform-aws-modules/alb/aws"
+  version    = "9.17.0"
 
-  name = "bfielder-alb-test"
-  vpc_id = module.vpc.vpc_id
+  name    = "bfielder-alb-test"
+  vpc_id  = module.vpc.vpc_id
   subnets = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+
+  listeners = {
+    http = {
+      port     = 80
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "bfielder-instance"
+      }
+    }
+  }
+
+
   target_groups = {
-      bfielder-instance = {
-      name_prefix      = "h1"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "ip"
+    bfielder-instance = {
+      name_prefix       = "h1"
+      protocol          = "HTTP"
+      port              = 80
+      target_type       = "ip"
       create_attachment = false
     }
   }
@@ -80,35 +95,36 @@ module "alb" {
 #Security Group
 module "ecs-security-group" {
   depends_on = [module.vpc]
-  source = "terraform-aws-modules/security-group/aws"
-  name = "bfielder-test-sg-ecs-deployment"
-  vpc_id = module.vpc.vpc_id
+  source     = "terraform-aws-modules/security-group/aws"
+  name       = "bfielder-test-sg-ecs-deployment"
+  vpc_id     = module.vpc.vpc_id
 
   ingress_with_source_security_group_id = [{
     source_security_group_id = module.alb-security-group.security_group_id
-    rule = "http-80-tcp"
-    }]
+    rule                     = "http-80-tcp"
+  }]
 
+  egress_rules = ["all-all"]
 }
 
 module "alb-security-group" {
   depends_on = [module.vpc]
-  source = "terraform-aws-modules/security-group/aws"
-  name = "bfielder-test-sg-alb-deployment"
-  vpc_id = module.vpc.vpc_id
+  source     = "terraform-aws-modules/security-group/aws"
+  name       = "bfielder-test-sg-alb-deployment"
+  vpc_id     = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules = ["http-80-tcp"]
-  egress_rules = ["all-all"]
-  
+  ingress_rules       = ["http-80-tcp"]
+  egress_rules        = ["all-all"]
+
 }
 
 
 
 module "ecs" {
   depends_on = [module.ecs-security-group]
-  source = "terraform-aws-modules/ecs/aws"
-  
+  source     = "terraform-aws-modules/ecs/aws"
+
   version = "6.3.0"
 
   cluster_name = "ecs-test-cluster-bfielder"
@@ -128,9 +144,11 @@ module "ecs" {
       cpu    = 1024
       memory = 4096
 
+      enable_cloudwatch_logging = false
+
       container_definitions = {
 
-        ecs-sample = {
+        bfielder-instance = {
           cpu       = 512
           memory    = 1024
           essential = true
@@ -142,37 +160,28 @@ module "ecs" {
               protocol      = "tcp"
             }
           ]
-          
+
           readonlyRootFilesystem = false
-
-          enable_cloudwatch_logging = false
-          memoryReservation = 100
+          memoryReservation      = 100
         }
-        logConfiguration = {
-            logDriver = "awslogs"
-              options = {
-                Name                    = "awslogs"
-                region                  = "eu-west-2"
-              }
-          }
-
       }
 
       load_balancer = {
         service = {
           target_group_arn = module.alb.target_groups["bfielder-instance"].arn
-          container_name   = "bfielder-ecs-test-lb"
+          container_name   = "bfielder-instance"
           container_port   = 80
         }
       }
 
       subnet_ids = module.vpc.private_subnets
       security_group_ingress_rules = {
-        alb_3000 = {
-          description                  = "Service port"
-          from_port                    = local.container_port
+        alb_http = {
+          description                  = "Allow traffic from ALB"
+          from_port                    = 80
+          to_port                      = 80
           ip_protocol                  = "tcp"
-          referenced_security_group_id = module.ecs-security-group.security_group_id
+          referenced_security_group_id = module.alb-security-group.security_group_id
         }
       }
       security_group_egress_rules = {
--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.11.0"
 
   backend "s3" {
-    bucket = "madetech-bfielder-tfstate-bucket"
-    key = "terraform.tfstate"
-    region = "eu-west-2"
+    bucket       = "madetech-bfielder-tfstate-bucket"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
     use_lockfile = true
   }
 
